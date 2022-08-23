fc63467b23c9a9fe5ade82e470353f35b74e00e9
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-08-23 09:04:51 +0700

    By Uunkn0wnN

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 
+
+
+const discord = require('discord.js-selfbot-v11');
+const fs = require('fs');
+const Eris = require("eris");
+const bot = new Eris(process.env.TOKEN);
+bot.connect();
+
+
+
+const client = new discord.Client();
+const keepAlive = require('./server.js');
+
+
+const events = fs.readdirSync('./events/');
+events.forEach(file => {
+	const eventname = file.split('.')[0];
+	const event = require(`./events/spotify`);
+	client.on(eventname, event.bind(null, client));
+});
+
+keepAlive();
+client.on("ready", () => {
+  console.log(`berhasil login ke ${client.user.username} !!`); 
+  });
+
+client.login(process.env.TOKEN);

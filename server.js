a37e402eaa82918e9c05f1b8debd82dbcc78f336
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2022-08-23 09:07:04 +0700

    By Uunkn0wnN

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require('express')
+const server = express();
+
+server.all("/", (req, res) => {
+  res.send('<meta http-equiv="refresh" content="0; URL=https://shiroweb-dashboard.221447.repl.co/"/>')
+})
+
+function keepAlive() {
+  server.listen(3000, () => {
+    console.log("by Uunkn0wnN")
+  })
+}
+module.exports = keepAlive
 
